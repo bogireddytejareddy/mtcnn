417fcd08d1da4f4c4d8f528c35dd5210c50a0304
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2019-07-13 20:20:41 +0530

    Update test.py

--- demo/test.py
+++ demo/test.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 from utils.detector import network_loader, MTCNNDetector
 
-
-
-if __name__ == '__main__':
+def test():
     pnet, rnet, onet = network_loader(p_model_path="./model_store/pnet_epoch_9.pt",
                                       r_model_path="./model_store/rnet_epoch_6.pt",
                                       o_model_path="./model_store/onet_epoch_3.pt")
@@ -12,4 +10,4 @@ if __name__ == '__main__':
     img = cv2.imread("./test.jpg")
     img_bg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     bboxs = mtcnn_detector.detect_face(img)
-    np.save('./bbox.npy', bboxs)
+    return bboxs
